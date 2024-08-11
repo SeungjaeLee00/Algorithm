e4f10cc61433c5d60632cafc729d726574ae9bd0
jaejae <86997274+SeungjaeLee00@users.noreply.github.com>
2024-08-11 10:27:54 +0900

    [Silver IV] Title: 보물, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.py
@@ -0,0 +1,12 @@
+n = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+S = 0
+
+for i in range(n):
+    S += min(A) * max(B)
+    A.pop(A.index(min(A)))
+    B.pop(B.index(max(B)))
+
+print(S)
